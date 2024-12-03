ffbb6fb308c3fb8db2cea8be900b67c964a037ae
LakshmikanthanC <geelakshmi2003@gmail.com>
2024-12-03 12:30:52 +0530

    updated package.json

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -33,7 +33,7 @@ const Hero =() => {
            <div className="flex items-center gap-3">
             <ButtonPrimary 
               label="Download CV"
-              href="/image/Resume.pdf"
+              href="/Portfolio/image/Resume.pdf"
               icon="download"
             />
 
@@ -48,7 +48,7 @@ const Hero =() => {
       <div className="hidden lg:block">
         <figure className="w-full max-w-[480px] ml-auto bg-gradient-to-t from-sky-400 via-25% via-sky-400/40 to-65% rounded-[60px] overflow-hidden">
           <img 
-          src="/images/hi.png" 
+          src="/Portfolio/images/hi.png" 
           width={656}
           height={800}
           alt="Lakshmikanthan" 
